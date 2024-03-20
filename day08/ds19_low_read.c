ea3a932ec6ef7de6cdbbc3300736fa2fd59fea03
RiverGang <sory304@naver.com>
2024-03-20 17:24:05 +0900

    240320 완료커밋

---
+++ day08/ds19_low_read.c
@@ -0,0 +1,29 @@
+// desc: 생성한 파일 data.txt에 저장된 데이터를 read 함수를 이용해 읽기
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <fcntl.h>
+ #include <unistd.h>
+ #define BUF_SIZE 100
+ void error_handling(char* message);
+
+ int main(void)
+ {
+   int fd;
+   char buf[BUF_SIZE];
+
+   fd=open("data.txt", O_RDONLY); // 읽기 전용으로 파일 열기
+
+   // 예외처리(리눅스에서 -1은 실패)
+   if(fd == -1)
+     error_handling("open() error!");
+   printf("file descriptor: %d \n", fd);
+
+   if(read(fd, buf, sizeof(buf))==-1) // read 함수이용, 11행에 선언된 buf에 읽어 들인 데이터 저장
+     error_handling("read() error!");
+
+   printf("file data: %s", buf); // buf 출력
+
+   close(fd);
+   return 0;
+ }
+
---
+++ day08/ds20_file04.c
@@ -0,0 +1,30 @@
+ /*
+ 파일 이름 : 이름.txt, data: 정보
+ 복사할 파일 이름: 이름copy.txt
+ */
+
+ #include <stdio.h>
+
+ void main()
+ {
+   char myInfo[30] = "LEESUNGHEE,201912186";
+   char myInfoCopy[30];
+
+   //파일생성
+   FILE* origen;
+   origen = fopen("이성희.txt", "w");
+   fprintf(origen, "%s", myInfo);
+   
+   
+   // 원본파일 데이터 읽기   
+   origen = fopen("이성희.txt", "r");
+   fscanf(origen, "%s", myInfoCopy);
+   fclose(origen);
+
+   FILE* copy;
+   copy = fopen("이성희copy.txt", "w");
+   fprintf(copy, "%s", myInfoCopy);
+
+   fclose(copy);
+ }
+
